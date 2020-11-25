506635662636b33a3f2fcf157eee7ff73b14d6cd
Alexandra R <alexandra.ricardel@gmail.com>
2020-11-25 02:14:49 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -2,9 +2,9 @@
  suite_a: "Amanda Presley", 
  suite_b: "Seymour Hoffman", 
  suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
+ suite_d: "Charlie Chaplin", 
+ suite_e: "Crumpet the Elf"
+ }
 
 def select_winner(passengers)
   winner = ""
